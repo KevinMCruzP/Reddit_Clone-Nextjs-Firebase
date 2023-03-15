356343b2dcaf55dc714363a97a44eb398e11249c
Kevin <kevin_mcp@outlook.com>
2023-03-15 12:59:21 -0300

    feat: Create post page and createpostlink component

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -1,4 +1,5 @@
 import { Community } from "@/src/atoms/communitiesAtom";
+import CreatePostLink from "@/src/components/Community/CreatePostLink";
 import { Header } from "@/src/components/Community/Header";
 import { CommunityNotFound } from "@/src/components/Community/NotFound";
 import PageContent from "@/src/components/Layout/PageContent";
@@ -21,7 +22,7 @@ export default function CommunityPage({ communityData }: CommunityPageProps) {
       <Header communityData={communityData} />
       <PageContent>
         <>
-          <div>LHS</div>
+          <CreatePostLink />
         </>
         <>
           <div>RHS</div>
