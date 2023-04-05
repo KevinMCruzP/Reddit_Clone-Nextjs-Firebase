b3f8538db661396657e30e2d44810fb301c4d4ac
Kevin <kevin_mcp@outlook.com>
2023-04-05 12:46:04 -0400

    feat: commenting on posts functionality

--- src/pages/r/[communityId]/comments/[pid].tsx
+++ src/pages/r/[communityId]/comments/[pid].tsx
@@ -1,10 +1,12 @@
 import { Post } from "@/src/atoms/postsAtom";
 import About from "@/src/components/Community/About";
 import PageContent from "@/src/components/Layout/PageContent";
+import Comments from "@/src/components/Posts/Comments/Comments";
 import PostItem from "@/src/components/Posts/PostItem";
 import { auth, firestore } from "@/src/firebase/clientApp";
 import useCommunityData from "@/src/hooks/useCommunityData";
 import usePosts from "@/src/hooks/usePosts";
+import { User } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
@@ -63,7 +65,11 @@ function PostPage() {
           />
         )}
         {/* <SelectedPost / */}
-        {/* <Comments /> */}
+        <Comments
+          user={user as User}
+          selectedPost={postStateValue.selectedPost}
+          communityId={postStateValue.selectedPost?.communityId as string}
+        />
       </>
       <>
         {communityStateValue.currentCommunity && (
